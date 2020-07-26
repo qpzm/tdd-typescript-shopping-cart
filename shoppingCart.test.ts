e747d8e3cde8b4b7d0ae6771d89943b541a3ebcc
qpzm <lhm1442@gmail.com>
2020-07-26 14:39:59 +0900

    Implement cart item CRUD

--- shoppingCart.test.ts
+++ shoppingCart.test.ts
@@ -16,5 +16,36 @@ describe('shopping cart', () => {
     expect(await cart.getItems()).toContainEqual(item);
   });
 
-  it.todo('lists items');
+  // Q. add하고 list로 테스트하면 결국 위의 테스트와 같은데?
+  // list는 따로 테스트 안 해도 될 것 같다. 테스트를 위해 쓰지도 않을
+  // initialize 함수를 추가할 필요는 없다.
+  xit('lists items', async () => {
+  });
+
+  it('finds an item given id', async () => {
+    const item = { id: '1111', name: 'Item1', price: 30 };
+    await cart.add(item);
+
+    expect(await cart.findItem(item.id)).toEqual(item);
+  });
+
+  it('removes a item given id', async () => {
+    const item = { id: '1111', name: 'Item1', price: 30 };
+    await cart.add(item);
+    await cart.removeItem(item.id);
+
+    expect(cart.getItems()).not.toContainEqual(item);
+  });
+
+  it('updates an item given id', async () => {
+    const item = { id: '1111', name: 'Item1', price: 30 };
+    await cart.add(item);
+    await cart.updateItem({
+      id: item.id,
+      price: 40,
+    });
+
+    const updatedItem = { ...item, ...{ price: 40 } };
+    expect(await cart.getItems()).toContainEqual(updatedItem);
+  });
 });
--- shoppingCart.ts
+++ shoppingCart.ts
@@ -2,6 +2,9 @@ export default function makeShoppingCart({ db }) {
   return Object.freeze({
     add,
     getItems,
+    removeItem,
+    findItem,
+    updateItem,
   })
 
   // Q. different functions for each cart or the same?
@@ -11,5 +14,17 @@ export default function makeShoppingCart({ db }) {
 
   async function getItems() {
     return db.list();
+  }
+
+  async function removeItem(id) {
+    return db.remove(id);
+  }
+
+  async function findItem(id) {
+    return db.findById(id);
+  }
+
+  async function updateItem(item) {
+    return db.update(item);
   }
 }
